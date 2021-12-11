e6f7e8793291f43ee0a34f6d2a91eddac728ffe9
墨墨 <36019799+1092682749@users.noreply.github.com>
2021-12-12 01:14:05 +0800

    delete configuration file from idea

--- src/test/java/com/example/demo/table/ClassToTable.java
+++ src/test/java/com/example/demo/table/ClassToTable.java
@@ -1,8 +1,12 @@
 package com.example.demo.table;
 
 import com.example.demo.model.*;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
+import org.springframework.data.annotation.Transient;
 
+import java.io.File;
 import java.lang.reflect.Field;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -14,6 +18,7 @@ public class ClassToTable {
         sql.append(tableName.toLowerCase() + "(\r\n");
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
+            if (field.isAnnotationPresent(Transient.class)) continue;
             String fieldName = field.getName();
             Class<?> type = field.getType();
             sql.append(fieldName.toLowerCase());
@@ -29,27 +34,54 @@ public class ClassToTable {
         System.out.println(sqlStr);
         return sqlStr;
     }
+
     public static String convertJDBCType(String javaType) throws Exception {
-        if (javaType.equals("String")){
+        if (javaType.equals("String")) {
             return " VARCHAR(225)";
-        } else if (javaType.equals("Integer") || javaType.equals("int") || javaType.equals("Long") || javaType.equals("long")){
+        } else if (javaType.equals("Integer") || javaType.equalsIgnoreCase("int") || javaType.equalsIgnoreCase("Long") || javaType.equalsIgnoreCase("long")) {
             return " int(12)";
-        } else if (javaType.equals("Short")) {
+        } else if (javaType.equalsIgnoreCase("Short")) {
             return " int(6)";
         } else if (javaType.equals("Date")) {
             return " datetime";
-        } else if (javaType.equals("Double")) {
+        } else if (javaType.equalsIgnoreCase("Double")) {
             return " DOUBLE(16,8)";
-        }else {
-            throw new Exception("没有考虑的数据类型"+javaType);
+        } else if (javaType.equalsIgnoreCase("byte")) {
+            return " BINARY(1)";
+
+        } else {
+            throw new Exception("没有考虑的数据类型" + javaType);
         }
     }
+
     public static void main(String[] args) throws Exception {
-        // String sql = create(.class);
         Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://dyzhello.club:3306/testredis?useUnicode=true&characterEncoding=utf-8","root","dyz");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/projects?useUnicode=true&characterEncoding=utf-8", "root", "root");
         Statement st = conn.createStatement();
-        // st.execute(sql);
+        System.out.println(File.separator);
+        System.out.println("com.example.demo.model".replace(".", File.separator));
+        URL resource = Thread.currentThread().getContextClassLoader().getResource("com/example/demo/model");
+        System.out.println(resource.getPath());
+        File file = new File(resource.getPath());
+        File[] files = file.listFiles();
+        for (File item : files) {
+            System.out.println(item.getName());
+            if (item.getName().contains("Example")) continue;
+            if (item.getName().contains("Admin")) continue;
+            if (item.getName().contains("Article")) continue;
+            int i = item.getName().indexOf(".");
+            String className = item.getName().substring(0, i);
+            Class<?> aClass = Class.forName("com.example.demo.model." + className);
+            String sql = create(aClass);
+            try {
+                st.execute(sql);
+            }catch (MySQLSyntaxErrorException e) {
+                System.err.println(e.getMessage());
+            }
+
+        }
+
+
         System.out.println("success!");
     }
 }
